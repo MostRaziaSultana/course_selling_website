bb92a3610e673657cb4ebbc19bf545beaf65a311
Most. Razia Sultana <raziasultana772233@gmail.com>
2025-05-10 14:26:35 +0600

    all blogs updated

--- Userdashboard/views.py
+++ Userdashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from Course.models import CoursePurchase
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def userdashboard(request):
-
-    return render(request, 'Dashboard/user_dashboard.html')
+    purchased_courses = CoursePurchase.objects.filter(user=request.user, is_ordered=True).select_related('course')
+    return render(request, 'Dashboard/user_dashboard.html', {
+        'purchased_courses': purchased_courses,
+    })
